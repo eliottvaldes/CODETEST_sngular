fb467b497098dc4bd68c881f0756bf898fa9455a
Eliot Valdes <eliottvaldes@hotmail.com>
2022-11-28 12:29:39 -0600

    add -> Results component implemented

--- src/components/SeriesResults.vue
+++ src/components/SeriesResults.vue
@@ -1,16 +1,28 @@
 <template>
-    <div>
-        
+    <div v-if="showResults">
+        El resultado de la serie cuando <var> n = {{ result.numberSerie }} </var> es:
+        <b> {{ result.result }} </b>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'SeriesResults',    
-    
+    name: 'SeriesResults',
+    props: {
+        result: {
+            type: Object,
+            required: true
+        },
+        showResults: {
+            type: Boolean,
+            default: false,
+            required: true
+        }
+    }
+
 }
 </script>
 
 <style scoped>
-    
+
 </style>
