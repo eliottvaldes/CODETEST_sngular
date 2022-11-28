a69ee9d96f176c14223f980178b17183ea17e47c
Eliot Valdes <eliottvaldes@hotmail.com>
2022-11-28 12:44:30 -0600

    upd -> styles_ implemented 4 page component

--- src/pages/SeriesPage.vue
+++ src/pages/SeriesPage.vue
@@ -1,23 +1,50 @@
 <template>
-    <div>
+    <div class="container">
 
-        <SeriesForm 
-            :isInputDisabled="isInputDisabled" 
-            @execute-serie="solveSerie" 
-        />
-        <br><br>
+        <nav class="navbar fixed-top navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    Eliot Valdes - Code Test
+                </a>
+            </div>
+        </nav>
 
-        <SeriesResults 
-            :showResults="showResults" 
-            :result="serieResult" 
-        />
-
-        <div v-if="tryAgain">
-            <button type="button" @click="resetApp">
-                Intentar de nuevo
-            </button>
+        <div class="row">
+            <div class="col-12">
+                <h1 class="heading-page">
+                    Reto Series
+                </h1>
+            </div>
         </div>
 
+        <div id="errorAlert"></div>
+
+        <div class="row justify-content-center">
+            <div class="col-11">
+                <div class="card p-4 rounded" id="container-components">
+                    <div class="row">
+                        <div class="col-12">
+                            <SeriesForm 
+                                :isInputDisabled="isInputDisabled" 
+                                @execute-serie="solveSerie" 
+                            />
+
+                            <br><br>
+
+                            <SeriesResults 
+                                :showResults="showResults" 
+                                :result="serieResult" 
+                            />
+                        </div>
+                        <div v-if="tryAgain" class="col-12">
+                            <button type="button" class="try-again" @click="resetApp">
+                                Intentar de nuevo
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -58,7 +85,7 @@ export default {
                     result: this.serieInstance.getSerie(),
                     numberSerie
                 };
-                
+
                 this.showResults = true
 
                 this.isInputDisabled = true
@@ -84,5 +111,22 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+    color: #fff;
+    font-size: 2.5rem;
+    font-weight: 700;
+    margin-bottom: 1.6rem;
+    margin-top: 90px;
+    text-align: center;
+}
+button.try-again{
+    float: right;    
+    margin-top: 25px;
+}
 
+#container-components{
+    border: 1px solid #f7b334;
+    /* set a shadow */
+    box-shadow: 0 0 7px #f7b334;
+}
 </style>
