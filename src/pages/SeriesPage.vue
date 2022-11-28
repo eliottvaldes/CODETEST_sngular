ac917010b1f174a7facadcd6fa85f96af46b5543
Eliot Valdes <eliottvaldes@hotmail.com>
2022-11-28 12:30:03 -0600

    add -> Pag/|logic component implemented

--- src/pages/SeriesPage.vue
+++ src/pages/SeriesPage.vue
@@ -1,24 +1,88 @@
 <template>
     <div>
-        
+
+        <SeriesForm 
+            :isInputDisabled="isInputDisabled" 
+            @execute-serie="solveSerie" 
+        />
+        <br><br>
+
+        <SeriesResults 
+            :showResults="showResults" 
+            :result="serieResult" 
+        />
+
+        <div v-if="tryAgain">
+            <button type="button" @click="resetApp">
+                Intentar de nuevo
+            </button>
+        </div>
+
     </div>
 </template>
 
 <script>
 
-import SeriesResults from '@/components/SeriesResults'
+import Series from '@/classes/Serie.class'
+
 import SeriesForm from '@/components/SeriesForm'
+import SeriesResults from '@/components/SeriesResults'
 
 export default {
     name: 'SeriesPage',
     components: {
-        SeriesResults,
-        SeriesForm
-    }
+        SeriesForm,
+        SeriesResults
+    },
+    data() {
+        return {
+            showError: false,
+            isInputDisabled: false,
+            showResults: false,
+            serieResult: {},
+            tryAgain: false,
+        }
+    },
+    computed: {
+        serieInstance() {
+            return new Series();
+        }
+    },
+    methods: {
+        solveSerie(numberSerie) {
+            try {
+
+                this.serieInstance.setNumber = numberSerie
+
+                this.serieResult = {
+                    result: this.serieInstance.getSerie(),
+                    numberSerie
+                };
+                
+                this.showResults = true
+
+                this.isInputDisabled = true
+                this.tryAgain = true
+
+            } catch (error) {
+                this.createErrorAlert(error)
+            }
+        },
+        resetApp() {
+            this.tryAgain = false
+            this.showResults = false
+            this.serieResult = {}
+            this.isInputDisabled = false
+        },
+        createErrorAlert(error) {
+            alert(error)
+        }
+
+    },
 
 }
 </script>
 
 <style scoped>
-    
+
 </style>
